a627a571558153c7aeebae5de2103a65a6fc322e
anooprac <anooprac@utexas.edu>
2023-10-21 22:47:27 -0500

    fixed matching - need to implement chaining

--- my_env/Matching.py
+++ my_env/Matching.py
@@ -8,6 +8,27 @@ import os
 WINDOW_SIZE = 2
 
 
+def map_sentences_to_slides(index, transcription_keyword_dict, slide_vector, wv, slide_to_sentence, slide_key):
+    sentence_list = list()
+    transcription_key_list = list(transcription_keyword_dict.keys())
+    index_moved = False
+    keep_going = True
+    for i in range(index[0] - WINDOW_SIZE, index[0] + WINDOW_SIZE):
+        if not keep_going:
+            break
+        if (i < 0 or i >= len(transcription_keyword_dict)):
+            continue
+        else:
+            keyword_list = transcription_keyword_dict[transcription_key_list[i]]
+            note_vector = [wv.get_vector(word) for word in keyword_list if wv.__contains__(word)]
+            if wv.n_similarity(slide_vector, note_vector) > .5:
+                sentence_list.append(transcription_key_list[i])
+                index[0] = index[0] + 1
+            else:
+                keep_going = False
+    return sentence_list
+
+
 def match(audio_file, pptx_file, embeddings):
     # audio_file = "./my_env/long.wav"
     # pptx_file = "./my_env/garbage_collection.pptx"
@@ -15,29 +36,20 @@ def match(audio_file, pptx_file, embeddings):
     body_text, slide_dict, title_dict = Transcription.SlidesScrape.extract_notes_from_slides(pptx_file)
     wv = KeyedVectors.load_word2vec_format('word2vec\\GoogleNews-vectors-negative300.bin', binary=True)
     slide_to_sentence = dict()
-    index = 0
+    index = list()
+    index.append(0)
     for slide_key in slide_dict:
         slide_to_sentence[slide_key] = list()
-        # slide_vector = [embeddings[tokens.index(word)] for word in slide_dict[slide_key][0]]
-        slide_vector = list()
-        for word in slide_dict[slide_key]:
-            if word in tokens:
-                slide_vector.append(embeddings[tokens.index(word)])
-        transcription_key_list = list(transcription_keyword_dict.keys())
-        for i in range(index - WINDOW_SIZE, index + WINDOW_SIZE):
-            if (i < 0 or i >= len(transcription_keyword_dict)):
-                continue
-            else:
-                keyword_list = transcription_keyword_dict[transcription_key_list[i]]
-                note_vector = list()
-                for word in keyword_list:
-                    if (word in tokens):
-                        note_vector.append(embeddings[tokens.index(word)])
-                if wv.n_similarity(slide_vector, note_vector) > .5:
-                    print("here")
-                    slide_to_sentence[slide_key].append(transcription_key_list[i])
-                    index += 1
-    print(slide_to_sentence, end="\n")
+        slide_vector = [wv.get_vector(word) for word in slide_dict[slide_key] if wv.__contains__(word)]
+        prev_index = index[0]
+        sentence_list = map_sentences_to_slides(index, transcription_keyword_dict, slide_vector, wv, slide_to_sentence, slide_key)
+        while (prev_index == index[0] and index[0] < len(transcription_keyword_dict)):
+            index[0] = index[0] + 1
+            prev_index = index[0]
+            sentence_list = map_sentences_to_slides(index, transcription_keyword_dict, slide_vector, wv, slide_to_sentence, slide_key)
+        slide_to_sentence[slide_key] = sentence_list
+    for key in slide_to_sentence.keys():
+        print(key + ": " + str(slide_to_sentence[key]))
                 
             
             
